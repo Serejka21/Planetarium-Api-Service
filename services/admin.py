c8a0d7d4e2f5b64fcdda125b5c382c240b473729
Serhii Buryk <sergmarten21@gmail.com>
2024-03-27 22:55:47 +0200

    update: register models to admin-panel

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    User,
+    PlanetariumDome,
+    ShowTheme,
+    AstronomyShow,
+    Reservation,
+    ShowSession, Ticket
+)
+
+admin.site.register(User)
+admin.site.register(PlanetariumDome)
+admin.site.register(ShowTheme)
+admin.site.register(AstronomyShow)
+admin.site.register(Reservation)
+admin.site.register(ShowSession)
+admin.site.register(Ticket)
